aee401c69cdd2f283fc477d20cd823d70068a845
14129883739 <14129883739@CBIFMSDM12.laboratorio.ifms.br>
2019-05-10 13:11:45 -0300

    novo

--- src/java/br/com/academia/mapeamento/pessoa/FuncionarioMapeamento.java
+++ src/java/br/com/academia/mapeamento/pessoa/FuncionarioMapeamento.java
@@ -5,10 +5,38 @@
  */
 package br.com.academia.mapeamento.pessoa;
 
+import br.com.academia.mapeamento.outros.CargoMapeamento;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  *
  * @author 14129883739
  */
-public class FuncionarioMapeamento {
+@Entity
+@Table(name="Funcionario")
+public class FuncionarioMapeamento extends PessoaMapeamento implements Serializable{
+  
+    private CargoMapeamento cargoMapeamneto;
+    private int satisfacaoCliente;
+
+    public CargoMapeamento getCargoMapeamneto() {
+        return cargoMapeamneto;
+    }
+
+    public void setCargoMapeamneto(CargoMapeamento cargoMapeamneto) {
+        this.cargoMapeamneto = cargoMapeamneto;
+    }
+
+    public int getSatisfacaoCliente() {
+        return satisfacaoCliente;
+    }
+
+    public void setSatisfacaoCliente(int satisfacaoCliente) {
+        this.satisfacaoCliente = satisfacaoCliente;
+    }
     
-}
+    
+    
+} 
